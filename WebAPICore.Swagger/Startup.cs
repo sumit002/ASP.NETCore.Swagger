cbd012f9ceefd8268b7951f3edf0474316ae3832
Sumit Das <summit.dash01@gmail.com>
2019-12-23 00:39:09 +0530

    Initial Commit for Swashbuckle implementation for .net api core with xml comments.

---
+++ WebAPICore.Swagger/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WebAPICore.Swagger
+{
+    public class Startup
+    {
+        #region Ctor
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        } 
+        #endregion
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Add Swagger Service Configuration to the Container
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1",
+                new Info() { Version = "v1", Title = "Web API Swagger", Description = "Web API Swagger Description" }));
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // Swagger Service Configuration
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API Swagger");
+                });
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            //app.UseRouting();
+            //app.UseAuthorization();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ WebAPICore.Swagger/WeatherForecast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebAPICore.Swagger
+{
+    /// <summary>
+    /// Weather Forecast Model Class
+    /// </summary>
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
